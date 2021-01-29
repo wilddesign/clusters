556b350585a6adc3c558cd4d9cc371fc5d9a794b
Wojciech Nogaś <wnappdv@gmail.com>
2021-01-29 22:55:55 +0100

    initial commit. changes added in pom.xml to make it work

---
+++ src/main/java/com/wilddesign/clusters/ClustersApplication.java
@@ -0,0 +1,13 @@
+package com.wilddesign.clusters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClustersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClustersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilddesign/clusters/ClustersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilddesign.clusters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClustersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
